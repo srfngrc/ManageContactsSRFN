bd1bbcdcb4d71d6065e3e863e857bdff59e7bf12
sg <srfngrc@outlook.com>
2019-07-22 17:38:00 -0400

    monday july22th 5h37pm

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -29,6 +29,13 @@
         private void InitializeComponent()
         {
             this.lblMessage = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Phone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblMessage
@@ -40,14 +47,64 @@
             this.lblMessage.TabIndex = 0;
             this.lblMessage.Text = "label1";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.FirstName,
+            this.LastName,
+            this.Email,
+            this.Phone});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 99);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(545, 194);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // FirstName
+            // 
+            this.FirstName.HeaderText = "First Name";
+            this.FirstName.Name = "FirstName";
+            this.FirstName.ReadOnly = true;
+            // 
+            // LastName
+            // 
+            this.LastName.HeaderText = "Last Name";
+            this.LastName.Name = "LastName";
+            this.LastName.ReadOnly = true;
+            // 
+            // Email
+            // 
+            this.Email.HeaderText = "Email Addr";
+            this.Email.Name = "Email";
+            this.Email.ReadOnly = true;
+            // 
+            // Phone
+            // 
+            this.Phone.HeaderText = "Phone Nbr";
+            this.Phone.Name = "Phone";
+            this.Phone.ReadOnly = true;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(221, 189);
+            this.ClientSize = new System.Drawing.Size(638, 364);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.lblMessage);
             this.Name = "Main";
             this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,5 +113,11 @@
         #endregion
 
         private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Phone;
     }
 }
--- Main.cs
+++ Main.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,12 +14,45 @@ namespace ContactManagerSRFN
     public partial class Main : Form
     {
         myInfo userInfo;
+        string ConnectionStringSRFN =
+            "Server=DatabaseSRFN.mssql.somee.com;" +
+                "Database=DatabaseSRFN;" +
+                "User Id=serafin;" +
+                "Password = 19771977; ";
         public Main(myInfo infoFromLogin)
         {
             InitializeComponent();
             userInfo = infoFromLogin;
             //Example of use
             lblMessage.Text = userInfo.loginMessage;
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            SqlConnection myconnection = new SqlConnection();
+            myconnection.ConnectionString = ConnectionStringSRFN;
+
+            try
+            {
+                myconnection.Open();
+
+                //this part changes accordingly
+                //Select
+                //update
+                //insert
+                //delete
+            }
+            catch (Exception ex123)
+            {
+                MessageBox.Show(ex123.Message);
+            }
+            finally
+            {
+                if (myconnection.State == ConnectionState.Open)
+                {
+                    myconnection.Close();
+                }
+            }
         }
     }
 }
