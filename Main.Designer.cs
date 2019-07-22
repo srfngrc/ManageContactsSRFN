1b5ae67f926303307c3b4a879ff576fa262902ef
sg <srfngrc@outlook.com>
2019-07-22 19:10:49 -0400

    july22th 7h11pm select for login WORKS and insert from Main WORKS so far

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -29,13 +29,22 @@
         private void InitializeComponent()
         {
             this.lblMessage = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgContacts = new System.Windows.Forms.DataGridView();
             this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Phone = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.LBLfirstname = new System.Windows.Forms.Label();
+            this.LBLlastname = new System.Windows.Forms.Label();
+            this.LBLphone = new System.Windows.Forms.Label();
+            this.LBLmail = new System.Windows.Forms.Label();
+            this.TBfirstname = new System.Windows.Forms.TextBox();
+            this.TBlastname = new System.Windows.Forms.TextBox();
+            this.TBphone = new System.Windows.Forms.TextBox();
+            this.TBmail = new System.Windows.Forms.TextBox();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgContacts)).BeginInit();
             this.SuspendLayout();
             // 
             // lblMessage
@@ -47,64 +56,152 @@
             this.lblMessage.TabIndex = 0;
             this.lblMessage.Text = "label1";
             // 
-            // dataGridView1
+            // dgContacts
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgContacts.AllowUserToAddRows = false;
+            this.dgContacts.AllowUserToDeleteRows = false;
+            this.dgContacts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgContacts.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ID,
             this.FirstName,
             this.LastName,
             this.Email,
             this.Phone});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 99);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(545, 194);
-            this.dataGridView1.TabIndex = 1;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
+            this.dgContacts.Location = new System.Drawing.Point(12, 99);
+            this.dgContacts.Name = "dgContacts";
+            this.dgContacts.ReadOnly = true;
+            this.dgContacts.Size = new System.Drawing.Size(545, 194);
+            this.dgContacts.TabIndex = 1;
             // 
             // ID
             // 
+            this.ID.DataPropertyName = "ID";
             this.ID.HeaderText = "ID";
             this.ID.Name = "ID";
             this.ID.ReadOnly = true;
             // 
             // FirstName
             // 
+            this.FirstName.DataPropertyName = "FirstName";
             this.FirstName.HeaderText = "First Name";
             this.FirstName.Name = "FirstName";
             this.FirstName.ReadOnly = true;
             // 
             // LastName
             // 
+            this.LastName.DataPropertyName = "LastName";
             this.LastName.HeaderText = "Last Name";
             this.LastName.Name = "LastName";
             this.LastName.ReadOnly = true;
             // 
             // Email
             // 
+            this.Email.DataPropertyName = "Email";
             this.Email.HeaderText = "Email Addr";
             this.Email.Name = "Email";
             this.Email.ReadOnly = true;
             // 
             // Phone
             // 
+            this.Phone.DataPropertyName = "Phone";
             this.Phone.HeaderText = "Phone Nbr";
             this.Phone.Name = "Phone";
             this.Phone.ReadOnly = true;
+            // 
+            // LBLfirstname
+            // 
+            this.LBLfirstname.AutoSize = true;
+            this.LBLfirstname.Location = new System.Drawing.Point(51, 330);
+            this.LBLfirstname.Name = "LBLfirstname";
+            this.LBLfirstname.Size = new System.Drawing.Size(54, 13);
+            this.LBLfirstname.TabIndex = 2;
+            this.LBLfirstname.Text = "FirstName";
+            // 
+            // LBLlastname
+            // 
+            this.LBLlastname.AutoSize = true;
+            this.LBLlastname.Location = new System.Drawing.Point(51, 359);
+            this.LBLlastname.Name = "LBLlastname";
+            this.LBLlastname.Size = new System.Drawing.Size(55, 13);
+            this.LBLlastname.TabIndex = 3;
+            this.LBLlastname.Text = "LastName";
+            // 
+            // LBLphone
+            // 
+            this.LBLphone.AutoSize = true;
+            this.LBLphone.Location = new System.Drawing.Point(51, 386);
+            this.LBLphone.Name = "LBLphone";
+            this.LBLphone.Size = new System.Drawing.Size(38, 13);
+            this.LBLphone.TabIndex = 4;
+            this.LBLphone.Text = "Phone";
+            // 
+            // LBLmail
+            // 
+            this.LBLmail.AutoSize = true;
+            this.LBLmail.Location = new System.Drawing.Point(51, 412);
+            this.LBLmail.Name = "LBLmail";
+            this.LBLmail.Size = new System.Drawing.Size(32, 13);
+            this.LBLmail.TabIndex = 5;
+            this.LBLmail.Text = "Email";
+            // 
+            // TBfirstname
+            // 
+            this.TBfirstname.Location = new System.Drawing.Point(135, 322);
+            this.TBfirstname.Name = "TBfirstname";
+            this.TBfirstname.Size = new System.Drawing.Size(100, 20);
+            this.TBfirstname.TabIndex = 6;
+            // 
+            // TBlastname
+            // 
+            this.TBlastname.Location = new System.Drawing.Point(135, 352);
+            this.TBlastname.Name = "TBlastname";
+            this.TBlastname.Size = new System.Drawing.Size(100, 20);
+            this.TBlastname.TabIndex = 7;
+            // 
+            // TBphone
+            // 
+            this.TBphone.Location = new System.Drawing.Point(135, 383);
+            this.TBphone.Name = "TBphone";
+            this.TBphone.Size = new System.Drawing.Size(100, 20);
+            this.TBphone.TabIndex = 8;
+            // 
+            // TBmail
+            // 
+            this.TBmail.Location = new System.Drawing.Point(135, 412);
+            this.TBmail.Name = "TBmail";
+            this.TBmail.Size = new System.Drawing.Size(100, 20);
+            this.TBmail.TabIndex = 9;
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(98, 444);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(75, 23);
+            this.btnAddNew.TabIndex = 10;
+            this.btnAddNew.Text = "Add New";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            this.btnAddNew.Click += new System.EventHandler(this.BtnAddNew_Click);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(638, 364);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(684, 479);
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.TBmail);
+            this.Controls.Add(this.TBphone);
+            this.Controls.Add(this.TBlastname);
+            this.Controls.Add(this.TBfirstname);
+            this.Controls.Add(this.LBLmail);
+            this.Controls.Add(this.LBLphone);
+            this.Controls.Add(this.LBLlastname);
+            this.Controls.Add(this.LBLfirstname);
+            this.Controls.Add(this.dgContacts);
             this.Controls.Add(this.lblMessage);
             this.Name = "Main";
             this.Text = "Main";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgContacts)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,11 +210,20 @@
         #endregion
 
         private System.Windows.Forms.Label lblMessage;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgContacts;
         private System.Windows.Forms.DataGridViewTextBoxColumn ID;
         private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
         private System.Windows.Forms.DataGridViewTextBoxColumn LastName;
         private System.Windows.Forms.DataGridViewTextBoxColumn Email;
         private System.Windows.Forms.DataGridViewTextBoxColumn Phone;
+        private System.Windows.Forms.Label LBLfirstname;
+        private System.Windows.Forms.Label LBLlastname;
+        private System.Windows.Forms.Label LBLphone;
+        private System.Windows.Forms.Label LBLmail;
+        private System.Windows.Forms.TextBox TBfirstname;
+        private System.Windows.Forms.TextBox TBlastname;
+        private System.Windows.Forms.TextBox TBphone;
+        private System.Windows.Forms.TextBox TBmail;
+        private System.Windows.Forms.Button btnAddNew;
     }
 }
--- Main.cs
+++ Main.cs
@@ -19,6 +19,8 @@ namespace ContactManagerSRFN
                 "Database=DatabaseSRFN;" +
                 "User Id=serafin;" +
                 "Password = 19771977; ";
+        private SqlConnection miconex;
+
         public Main(myInfo infoFromLogin)
         {
             InitializeComponent();
@@ -27,7 +29,7 @@ namespace ContactManagerSRFN
             lblMessage.Text = userInfo.loginMessage;
         }
 
-        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void GetContacts()
         {
             SqlConnection myconnection = new SqlConnection();
             myconnection.ConnectionString = ConnectionStringSRFN;
@@ -35,12 +37,51 @@ namespace ContactManagerSRFN
             try
             {
                 myconnection.Open();
-
                 //this part changes accordingly
                 //Select
                 //update
                 //insert
                 //delete
+
+                //Step 1: Define the query
+                string query = "SELECT * FROM Contact ORDER BY LastName";
+
+                //Step 2: Create and inizializate the command object 
+                SqlCommand myCommand = new SqlCommand();
+                myCommand.CommandText = query;
+                myCommand.Connection = myconnection;
+                //Step 3: Create datatable that will contain the data
+                DataTable myTable = new DataTable();
+                //myTable.Columns.Add("ID");
+                //myTable.Columns.Add(new DataColumn("FirstName"));
+                //myTable.Columns.Add(new DataColumn("LastName"));
+                ////myTable.Columns.Add(new DataColumn("ID"));
+                //myTable.Columns.Add(new DataColumn("Phone"));
+                //myTable.Columns.Add(new DataColumn("Email"));
+
+                //Step 4: Fill in the datatable with information from the DataBase
+                SqlDataReader myReader = myCommand.ExecuteReader();
+
+                myTable.Load(myReader);
+                dgContacts.DataSource = myTable;
+
+
+
+                //while (myReader.Read())
+                //{
+                //    DataRow contact = myTable.NewRow();
+                //    contact["ID"] = myReader["ID"];
+                //    contact["FirstName"] = myReader["FirstName"];
+                //    contact["LastName"] = myReader["LastName"];
+                //    contact["Phone"] = myReader["Phone"];
+                //    contact["Email"] = myReader["Email"];
+                //    myTable.Rows.Add(contact);
+                //}
+
+                ////Step 5: Bind datatable to datagridview
+                ////IMPORTANTTTTTTTTTTTTTTTTTTTTTTT
+                //dgContacts.DataSource = myTable;
+
             }
             catch (Exception ex123)
             {
@@ -53,6 +94,71 @@ namespace ContactManagerSRFN
                     myconnection.Close();
                 }
             }
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            GetContacts();
+
+        }
+
+        private void BtnAddNew_Click(object sender, EventArgs e)
+        {
+
+            string InsertQuery = "INSERT INTO Contact (FirstName, LastName, Phone, Email)" +
+                " VALUES ('{0}','{1}','{2}','{3}')";
+            InsertQuery = string.Format(InsertQuery,
+                TBfirstname.Text,
+                TBlastname.Text,
+                TBphone.Text,
+                TBmail.Text);
+
+            SqlConnection miconex = new SqlConnection(ConnectionStringSRFN);
+
+            try
+            {
+                miconex.Open();
+
+                if (TBfirstname.Text == string.Empty)
+                {
+                    MessageBox.Show("Please enter a name");
+                    return;
+                }
+                if (TBlastname.Text == string.Empty)
+                {
+                    MessageBox.Show("Please enter a last name");
+                    return;
+                }
+                if (TBphone.Text == string.Empty)
+                {
+                    MessageBox.Show("Please enter a phone");
+                    return;
+                }
+                if (TBmail.Text == string.Empty)
+                {
+                    MessageBox.Show("Please enter an email");
+                    return;
+                }
+
+                SqlCommand micommand = new SqlCommand(InsertQuery, miconex);
+                micommand.ExecuteNonQuery();
+            }
+            catch (Exception aaa)
+            {
+                MessageBox.Show(aaa.Message);
+            }
+            finally
+            {
+                if (miconex.State == ConnectionState.Open)
+                {
+                    miconex.Close();
+                    GetContacts();
+                }
+            }
+            TBfirstname.Text = string.Empty;
+            TBlastname.Text = string.Empty;
+            TBphone.Text = string.Empty;
+            TBmail.Text = string.Empty;
         }
     }
 }
